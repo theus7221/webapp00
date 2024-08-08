745e3212d356fd50aef76621b80350f2b645d775
theus7221 <matheusvsantos123@gmail.com>
2024-08-08 09:50:59 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,7 +3,7 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.title("Olá, Massaki!")
+st.title("Olá, Theteu e pepe!")
 st.header("Turma 2ei-sesi")
 st.subheader("Este pessoal é fera")
 st.write("Vamos la, pessoal")             
